deed7711e5b2d86e4054e2a248c26c1c0f0a8625
Josh236061 <71394743+Josh236061@users.noreply.github.com>
2022-11-07 22:26:15 -0600

    Finalized Lesson 1 Code

--- controllers/index.js
+++ controllers/index.js
@@ -3,6 +3,10 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 
+const homeRoutes = require('./home-routes.js');
+
+router.use('/', homeRoutes);
+
 router.use('/api', apiRoutes);
 
 router.use((req, res) => {
--- server.js
+++ server.js
@@ -1,17 +1,23 @@
+const path = require('path');
 const express = require('express');
-const routes = require('./routes');
-const sequelize = require('./config/connection');
+const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const sequelize = require('./config/connection');
+
+const hbs = exphbs.create({});
+ 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-// turn on routes
-app.use(routes); 
+app.use(require('./controllers/'));
 
-// turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
