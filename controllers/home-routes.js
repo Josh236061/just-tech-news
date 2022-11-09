304305039711e01101c7ef640730519f715ae815
Josh236061 <71394743+Josh236061@users.noreply.github.com>
2022-11-08 18:58:39 -0600

    Updated 'javascript' files

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -38,6 +38,12 @@ router.get('/', (req, res) => {
       console.log(err);
       res.status(500).json(err);
     });
+
 });
+
+router.get('/login', (req, res) => {
+  res.render('login');
+});
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
+const session = require('express-session');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
 
 const app = express();
 const PORT = process.env.PORT || 3001;
